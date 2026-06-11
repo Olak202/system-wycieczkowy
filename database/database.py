46411cee4ef90572544c2882c4b54ef2db20da6a
aleks <aleksandra.krauze@student.wat.edu.pl>
2026-06-12 01:17:13 +0200

    Dodano baze danych SQLite

---
+++ database/database.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+conn = sqlite3.connect("wycieczki.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS klienci (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    imie TEXT NOT NULL,
+    nazwisko TEXT NOT NULL,
+    x REAL,
+    y REAL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS biura (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nazwa TEXT NOT NULL,
+    miasto TEXT,
+    x REAL,
+    y REAL
+)
+""")
+
+conn.commit()
+conn.close()
+
+print("Baza danych utworzona")
